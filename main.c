32e580efe8e898fbabb8c20a413a20804285a927
gky <gkycl@163.com>
2016-05-26 13:15:49 +0800

    add the main function

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
+
+int RecvArg(int fd, char *buffer, int *pos)
+{
+    char c;
+    int c_pos = 0;
+    int t_num = 0;
+
+    while(read(fd, &c, 1) > 0) {
+        if ()
+    }
+}
